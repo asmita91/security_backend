2348e2402bb5c8401941e976d57cec0888aeb149
asm <94774188+asmita91@users.noreply.github.com>
2024-08-15 11:36:05 +0545

    user routes issue

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,56 +1,9 @@
 
-// const express = require('express');
-
-// const router = express.Router();
-// const userController = require('../controllers/user_controller');
-// const { verifyUser } = require('../middlewares/auth');
-
-// const logRequest = require('../middlewares/logRequest');  // Import the logging middleware
-
-
-// router.route('/register')
-//     .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
-//     .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
-//     .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
-//     .post(logRequest, userController.userRegister);
-
-// router.route('/login')
-//     .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
-//     .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
-//     .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
-//     .post(logRequest, userController.userLogin);
-
-// router.route('/lockAccount')
-//     .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
-//     .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
-//     .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
-//     .post(logRequest, userController.userAccountLock);
-
-
-// // get,update and delete profile/account
-// router.route('/')
-//     .post((req, res, next) => res.status(405).json({ error: "POST method is not allowed" }))
-//     .get(logRequest,verifyUser, userController.getProfile)
-//     .delete(logRequest,verifyUser, userController.deleteAccount)
-//     .put(logRequest, verifyUser, userController.updateProfile);
-
-
-// router.get('/passwordNeedChange', logRequest, verifyUser, userController.getPasswordExpiry);
-// router.put('/changePassword', logRequest, verifyUser, userController.changePassword);
-
-// router.post("/forgot-password", logRequest, userController.forgotPassword);
-// router.put("/password/reset/:token",logRequest, userController.resetPassword);
-
-// module.exports = router;
-
-
-
-
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user_controller');
 const { verifyUser } = require('../middlewares/auth');
-const logRequest = require('../middlewares/logRequest');  // Import the logging middleware
+const logRequest = require('../middlewares/logRequest');  
 
 router.route('/register')
     .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
@@ -81,5 +34,9 @@ router.put('/changePassword', logRequest, verifyUser, userController.changePassw
 
 router.post("/forgot-password", logRequest, userController.forgotPassword);
 router.put("/password/reset/:token", logRequest, userController.resetPassword);
+
+
+router.post('/send_otp', userController.sendOtp);
+
 
 module.exports = router;
