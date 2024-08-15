6db2d56bdd1a09047f7dacd59bd95851b010993b
asm <94774188+asmita91@users.noreply.github.com>
2024-08-15 11:34:56 +0545

    Log related schema problem

--- models/User.js
+++ models/User.js
@@ -1,110 +1,4 @@
 
-// const mongoose = require('mongoose');
-// const userSchema = new mongoose.Schema({
-
-//     fullName: {
-//         type: String,
-//         required: true,
-//         minLength: [2, 'Length of full name cannot be smaller than 2']
-//     },
-
-//     role: {
-//         type: String,
-//         enum: ['user', 'admin'],
-//         default: 'user',
-
-//     },
-//     email: {
-//         type: String,
-//         unique: true,
-//         minLength: [6, 'Length of email cannot be smaller than 6'],
-//         required: true
-//     },
-
-//     password: {
-//         type: String,
-//         minLength: [8, 'Length of password cannot be smaller than 8'],
-//         required: true
-//     },
-//     picture: {
-//         type: String,
-//         default: 'defaultImage.jpg'
-
-//     },
-//     passwordLastChanged: {
-//         type: Date,
-//         default: Date.now,
-//     },
-//     lockUntil: Date ,
-
-
-//     passwordHistory: [],
-
-//     // Note: STATUS is disable when the users enter the wrong password for more than 3 times
-//     //  , otherwise, it is active
-//     status: {
-//         type: String,
-//         enum: ['active', 'disable'],
-//         default: 'active'
-//     },
-
-// }, { timestamps: true });
-
-// userSchema.set('toJSON', {
-//     transform: (document, returnedDocument) => {
-//         returnedDocument.id = document._id.toString();
-
-//         delete returnedDocument._id;
-//         delete returnedDocument.__v;
-//         delete returnedDocument.password;
-//     }
-// });
-
-
-// // method for returning bool to check whether password is need to be changed in every 90 days or not
-// const isPasswordChangeRequired = function (lastPasswordChangedDate) {
-//     const expirationDays = 90; // Change password every 90 days
-//     const lastChanged = lastPasswordChangedDate;
-//     console.log(`Last password changed date: ${lastChanged}`)
-//     const today = new Date();
-//     const expirationDate = new Date(lastChanged);
-//     expirationDate.setDate(expirationDate.getDate() + expirationDays);
-//     return today > expirationDate;
-// };
-
-// // password guidelines => must be 8 < x < 12, 1 upper case, 1 lowercase, 1 number , 1 special character
-
-// const validatePassword = (password) => {
-//     const isLengthValid = password.length >= 8 && password.length <= 12;
-//     const isUppercaseValid = /[A-Z]/.test(password);
-//     const isLowercaseValid = /[a-z]/.test(password);
-//     const isNumberValid = /\d/.test(password);
-//     const isSpecialCharValid = /[!@#$%^&*()_+[\]{};':"<>?~]/.test(password);
-
-//     return isLengthValid && isUppercaseValid && isLowercaseValid && isNumberValid && isSpecialCharValid ? true : false;
-// };
-
-
-// userSchema.methods.getResetPasswordToken = function () {
-//     const resetToken = crypto.randomBytes(20).toString("hex");
-  
-//     console.log(resetToken);
-  
-//     this.resetPasswordToken = crypto
-//       .createHash("sha256")
-//       .update(resetToken)
-//       .digest("hex");
-  
-//     console.log(this.resetPasswordToken);
-//     this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
-  
-//     return resetToken;
-//   };
-
-// const User = new mongoose.model('User', userSchema);
-
-// module.exports = { User, userSchema, isPasswordChangeRequired, validatePassword };
-
 
 
 
@@ -114,7 +8,7 @@ const userSchema = new mongoose.Schema({
     fullName: {
         type: String,
         required: true,
-        minLength: [2, 'Length of full name cannot be smaller than 2']
+        minLength: [5, 'Length of full name cannot be smaller than 5']
     },
     role: {
         type: String,
@@ -142,6 +36,10 @@ const userSchema = new mongoose.Schema({
     },
     lockUntil: {
         type: Date
+    },
+    otp:{
+type: String, 
+default: null,
     },
     failedAttempts: {
         type: Number,
@@ -173,7 +71,6 @@ userSchema.set('toJSON', {
 const isPasswordChangeRequired = function (lastPasswordChangedDate) {
     const expirationDays = 90; 
     const lastChanged = lastPasswordChangedDate;
-    console.log(`Last password changed date: ${lastChanged}`)
     const today = new Date();
     const expirationDate = new Date(lastChanged);
     expirationDate.setDate(expirationDate.getDate() + expirationDays);
--- models/log.js
+++ models/log.js
@@ -1,18 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const logSchema = new mongoose.Schema({
-//     time: { type: Date, default: Date.now },
-//     username: { type: String, required: true },
-//     url: { type: String, required: true },
-//     method: { type: String, required: true },
-//     role: { type: String, required: true },
-//     status: { type: String, required: true },
-// }, { timestamps: true });
-
-// const Log = mongoose.model('Log', logSchema);
-
-// module.exports = Log;
-
 
 
 const mongoose = require('mongoose');
@@ -29,3 +14,7 @@ const logSchema = new mongoose.Schema({
 const Log = mongoose.model('Log', logSchema);
 
 module.exports = Log;
+
+
+
+
