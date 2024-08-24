b6b31715da80a4e7fa2521f69a5c2123a83b8347
asm <94774188+asmita91@users.noreply.github.com>
2024-08-25 03:27:30 +0545

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 
 
 app.use(cors({
-    origin: 'https://localhost:3000', 
+    origin: 'https://security-backend.vercel.app/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, 
